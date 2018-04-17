70f2063383117140309d61c14d9c3b062d334e1b
Craig Conover <cvconover@gmail.com>
2018-04-17 13:43:03 -0700

    added more docs/comments

--- samples/webhooks/presence/server.js
+++ samples/webhooks/presence/server.js
@@ -1,3 +1,18 @@
+/*
+  This code is provided "as-is" with no implied warranty/support/etc. Use at
+  your own risk and customize to your requirements as needed.
+
+  Below, each event has a sepaare path but PubNub Presence Web Hooks can be
+  configured so that each event has a separeate path (/active, /join, etc.)
+  or one path to handle them all and you parse the action in your handler.
+  But it is highly recommended that you have at least two web hook paths:
+    - one for active/inactive
+    - one for join/leave/timeout/statechange
+  since active/inactive are multipart form (non-JSON) and the other
+   are application/json format (see code below for clarity).
+   Questions? Contact PubNub Support: https://support.pubnub.com
+*/
+
 var express = require("express");
 var inspect  = require('util').inspect;
 
@@ -10,30 +25,44 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
+// JOIN Web Hook URI = https://myserver.com:3000/join
 app.post("/join", (request, response) => {
     console.log("JOIN - request.body: ", request.body);
+    // do something but do not delay the 200 response or PubNub will try calling
+    //   again after 5s of no response up to 3 times before it quits trying
     response.status(200).end();
 });
 
+// leave Web Hook URI = https://myserver.com:3000/leave
 app.post("/leave", (request, response) => {
     console.log("LEAVE - request.body: ", request.body);
+    // do something but do not delay the 200 response or PubNub will try calling
+    //   again after 5s of no response up to 3 times before it quits trying
     response.status(200).end();
 });
 
+// timeout Web Hook URI = https://myserver.com:3000/timeout
 app.post("/timeout", (request, response) => {
     console.log("TIMEOUT - request.body: ", request.body);
+    // do something but do not delay the 200 response or PubNub will try calling
+    //   again after 5s of no response up to 3 times before it quits trying
     response.status(200).end();
 });
 
+// statechange Web Hook URI = https://myserver.com:3000/statechange
 app.post("/statechange", (request, response) => {
     console.log("STATE CHANGE - request.body: ", request.body);
+    // do something but do not delay the 200 response or PubNub will try calling
+    //   again after 5s of no response up to 3 times before it quits trying
     response.status(200).end();
 });
 
+// active Web Hook URI = https://myserver.com:3000/active
 app.post("/active", (req, res) => {
   parseMultiPart("ACTIVE", req, res);
 });
 
+// inactive Web Hook URI = https://myserver.com:3000/inactive
 app.post("/inactive", (req, res) => {
   parseMultiPart("INACTIVE", req, res);
 });
